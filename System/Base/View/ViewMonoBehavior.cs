94507702bd1b80c1c62ebd9b786f1d694c72c8ce
d.bogdanov <d.bogdanov@nexters.com>
2024-09-21 14:05:32 +0400

    change unity view for new base class

--- System/Base/View/ViewMonoBehavior.cs
+++ System/Base/View/ViewMonoBehavior.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using Disposable;
 using MVVM.MVVM.System.Base.ViewModel;
-using UnityEngine;
 
 namespace MVVM.MVVM.System.Base.View
 {
-public abstract class ViewMonoBehavior<TViewModel> : MonoBehaviour, IView, IDisposable where TViewModel : IViewModel
+public abstract class ViewMonoBehavior<TViewModel> : MonoBehaviourDisposable, IView where TViewModel : IViewModel
 {
-    protected bool disposed = false;
-    protected bool isDestroyed = false;
     protected TViewModel viewModel;
     public virtual void Initialize(TViewModel viewModel)
     {
@@ -17,43 +14,6 @@ public abstract class ViewMonoBehavior<TViewModel> : MonoBehaviour, IView, IDisp
     ~ViewMonoBehavior()
     {
         Dispose(false);
-    }
-    
-    public virtual void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposed)
-        {
-            return;
-        }
-
-        if (disposing)
-        {
-            DisposeManagedResources();
-        }
-
-        DisposeUnmanagedResources();
-
-        if (!isDestroyed)
-        {
-            Destroy(gameObject);
-            isDestroyed = true;
-        }
-        
-        disposed = true;
-    }
-
-    protected virtual void DisposeManagedResources()
-    {
-    }
-
-    protected virtual void DisposeUnmanagedResources()
-    {
     }
 }
 }
